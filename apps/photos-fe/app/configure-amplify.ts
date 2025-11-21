1372b03db1407f020d6ad26ec31cd8d0194fd79b
gustavoch <gustavo.francelino@gmail.com>
2025-11-21 13:12:03 -0300

    fix outputs

--- apps/photos-fe/app/configure-amplify.ts
+++ apps/photos-fe/app/configure-amplify.ts
@@ -1,4 +1,4 @@
-import BEStack from '../outputs.json';
+import BEStack from '../api-stack.outputs.json';
 import { Amplify } from 'aws-amplify';
 import { fetchAuthSession } from 'aws-amplify/auth';
 

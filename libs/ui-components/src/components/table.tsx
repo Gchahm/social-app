d8766f7343da70006f60f827c00617b8fad506c2
gustavoch <gustavo.francelino@gmail.com>
2025-11-21 23:07:00 -0300

    fix lint

--- libs/ui-components/src/components/table.tsx
+++ libs/ui-components/src/components/table.tsx
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 
-import { cn } from '@chahm/ui-components/utils';
+import { cn } from '../utils';
 
 function Table({ className, ...props }: React.ComponentProps<'table'>) {
   return (
